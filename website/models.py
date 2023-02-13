501c71b97c88430764fea093c346c8d81a80c936
holycactusjuice <patthepatat@gmail.com>
2023-02-12 21:13:24 -0500

    first commit

---
+++ website/models.py
@@ -0,0 +1,12 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+from sqlalchemy.dialects.postgresql import JSONB
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    username = db.Column(db.String(20))
+    data = db.Column(JSONB)
